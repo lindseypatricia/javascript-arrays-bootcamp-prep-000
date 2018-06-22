693daa58807ab06d1b2ad24b2f158ea97cee44f0
Lindsey Brown <brown.lindsey.p@gmail.com>
2018-06-22 02:09:35 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+  ]
+  
+  function destructivelyAddElementToBeginningOfArray(foo) {
+    var array = [1]
+    array.unshift("foo");
+    return array
+  }
+ 
