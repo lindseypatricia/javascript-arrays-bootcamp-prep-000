09e57ae44b1d4c2abe0972cde87b3559d8ac2a5f
Lindsey Brown <brown.lindsey.p@gmail.com>
2018-06-22 03:30:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,6 +39,6 @@ var chocolateBars = [
    return array
  }
  function removeElementFromEndOfArray(array) {
-   array.slice(0,array.length - 1)
+   array.slice(0, array.length - 1)
    return array
  }
